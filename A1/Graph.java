ff00ba5b5d1ffda39c4bc8b2ce33d03d7faac248
Bill Xiong <billxiong99@gmail.com>
2014-05-22 16:17:34 -0700

    Added A1, started experimenting with coding

---
+++ A1/Graph.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Stack;
+import java.util.Queue;
+
+public class Graph
+{
+
+	private Vertex root;
+	private float[][] adjMatrix;
+
+	public Graph(Vertex n){
+		this.root = n;
+	}
+
+	//connects the two vertex
+	public void connect(Vertex a, Vertex b){
+
+	}
+
+	public void connect(Vertex a, Vertex b, int weight){
+
+	}
+}
+
