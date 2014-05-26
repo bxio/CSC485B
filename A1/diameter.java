9432bdd655772ae6b1e93cd654d217219127e3c9
Bill Xiong <billxiong99@gmail.com>
2014-05-26 16:12:29 -0700

    Added documentation.

--- A1/diameter.java
+++ A1/diameter.java
@@ -1,17 +1,24 @@
 /*********************
 ** Bill Xiong       **
 ** V00737042        **
-** CSC485B          **
+** CSC485B - A1     **
 ** Summer 2014      **
 *********************/
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.*;
 
 public class diameter{
 
-	//scan in the file?
-	public static void parseFile(String filename, float[][] adj_list, int size){
+	/**
+	 * Parses the csv file into a weighted adjacency matrix.
+	 *
+	 * @param  filename  the name of the file, provided by the user
+	 * @param  adj_matrix  the adjacency matrix to be populated
+	 * @param  size  the size of the adjacency matrix for the graph in question
+	 */
+	 public static void parseFile(String filename, float[][] adj_matrix, int size){
 		File file = new File(filename);
 
 		try{
@@ -26,10 +33,10 @@ public class diameter{
 				String[] nodes = line.split(";");
 
 				for(int j = 0; j < col; j++){
-					adj_list[i][j] = Float.parseFloat(nodes[j+1]);
+					adj_matrix[i][j] = Float.parseFloat(nodes[j+1]);
 
-					if(adj_list[i][j] == 0){
-						adj_list[i][j] = Float.POSITIVE_INFINITY;
+					if(adj_matrix[i][j] == 0){
+						adj_matrix[i][j] = Float.POSITIVE_INFINITY;
 					}
 				}
 			}
@@ -40,34 +47,52 @@ public class diameter{
 
 	}
 
+	/**
+	 * Returns the size of the adjacency matrix to be generated from parsing
+	 * the .csv file.
+	 *
+	 * @param  firstline  the first line of the csv file, separated by semicolons
+	 * @return      the maximum lenght and width needed to fit the csv into an adjacency matrix.
+	 */
 	public static int findSize(String firstLine){
 		String[] nodes = firstLine.split(";");
 		return nodes.length;
 	}
 
-	//Runs the Floyd Warshall algorithm on the given adjacency list
-	public static void floydWarshall(float[][] adj_list, int size){
+	/**
+	 * Runs the Floyd Warshall algorithm on the given adjacency list
+	 *
+	 * @param  adj_matrix  the adjacency matrix representing the graph
+	 * @param  size  the size of the adjacency matrix (it's offset by 1, rememeber.)
+	 */
+	public static void floydWarshall(float[][] adj_matrix, int size){
 		for(int k = 0; k < size; k++){
 			for(int i = 0; i < size; i++){
 				for(int j = 0; j < size; j++){
-					adj_list[i][j] = Math.min(adj_list[i][j], adj_list[i][k] + adj_list[k][j]);
+					adj_matrix[i][j] = Math.min(adj_matrix[i][j], adj_matrix[i][k] + adj_matrix[k][j]);
 				}
 			}
 		}
 	}
 
-	public static int getDiameter(float[][] adj_list, int size){
+	/**
+	 * Finds the diameter of the graph
+	 *
+	 * @param  adj_matrix  the adjacency matrix representing the graph
+	 * @return      the diameter of the graph.
+	 */
+	public static int getDiameter(float[][] adj_matrix, int size){
 		//copy the adj matrix
-		float[][] copyOfAdjMatrix = adj_list.clone();
+		float[][] copyOfAdjMatrix = adj_matrix.clone();
 		//run the floyd-warshall algorithm
 		floydWarshall(copyOfAdjMatrix, size);
 
 		float diameter = 0;
 		for(int i = 0; i < size; i++){
 			for(int j = 0; j < size; j++){
-				if(adj_list[i][j] != Float.POSITIVE_INFINITY){
-					if (diameter < adj_list[i][j]){
-						diameter = adj_list[i][j];
+				if(adj_matrix[i][j] != Float.POSITIVE_INFINITY){
+					if (diameter < adj_matrix[i][j]){
+						diameter = adj_matrix[i][j];
 					}
 				}
 			}
@@ -86,7 +111,7 @@ public class diameter{
 			File file = new File(filename);
 			sc = new Scanner(file);
 			String line = sc.nextLine();
-			System.out.println("First line:"+line);
+			//System.out.println("First line:"+line);
 			//file exists, let's find out size of the adj matrix.
 			int size = findSize(line);
 			System.out.println("Size is:"+size);
