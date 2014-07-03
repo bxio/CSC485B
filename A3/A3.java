5e4da7f5af17263ff62b10136226d6351f16a7f5
Bill Xiong <billxiong99@gmail.com>
2014-07-02 18:02:26 -0700

    More progress

--- A3/A3.java
+++ A3/A3.java
@@ -56,14 +56,29 @@ public class A3{
     }
   }
 
+  public static void makeRandomFirstAdopters(int numToAdopt, boolean[] status_matrix){
+    for(int i=0;i<numToAdopt;i++){
+      status_matrix[i] = true;
+    }
+    //TODO: Find minimal case.
+  }
+
+  public static void printAdoptionStatus(boolean[] status_matrix){
+    for(int i=0;i<status_matrix.length;i++){
+      System.out.print(status_matrix[i]+",");
+    }
+
+    System.out.print("\n");
+  }
 
   public static void main (String[] args){
     if(args.length == 0){
       System.out.println("Usage: java A3 location_of_file.csv <q value>");
     }else{
       String csvOfGraph = args[0];
-      float qValue = Float.parseFloat(args[1]);
-      System.out.println("Now analyzing '"+csvOfGraph +"' with q-value of '"+qValue+ "'.");
+      int numAdopters = Integer.parseInt(args[1]);
+      float qValue = Float.parseFloat(args[2]);
+      System.out.println("Now analyzing '"+csvOfGraph +"' with "+numAdopters+" adoptors and q-value of '"+qValue+ "'.");
       try{
         File file = new File(csvOfGraph);
         Scanner sc = new Scanner(file);
@@ -72,9 +87,12 @@ public class A3{
         System.out.print("Size of graph is: "+size+"\n");
         //Create the adj matrix
         float[][] adj_matrix = new float[size][size];
-        Node[] node_matrix = new Node[size];
+        boolean[] status_matrix = new boolean[size];
         //populate the adj matrix
         parseFile(csvOfGraph, adj_matrix, size);
+        makeRandomFirstAdopters(2,status_matrix);
+
+        printAdoptionStatus(status_matrix);
 
       }catch(FileNotFoundException e){
         System.out.println("File not found error.");
